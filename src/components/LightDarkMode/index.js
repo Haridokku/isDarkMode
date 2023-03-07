f5063c7d218da75e9b2cbe6f053fc7b616e048db
Haridokku <124425442+Haridokku@users.noreply.github.com>
2023-03-07 12:21:38 +0530

    final change

--- src/components/LightDarkMode/index.js
+++ src/components/LightDarkMode/index.js
@@ -1,32 +1,26 @@
-// Write your code here
 import {Component} from 'react'
 
 import './index.css'
 
 class LightDarkMode extends Component {
-  state = {isDarkMode: true}
-  backgroundChange = () => {
+  state = {
+    isDarkMode: true,
+  }
+
+  onClickButton = () => {
     this.setState(prevState => ({isDarkMode: !prevState.isDarkMode}))
   }
-  buttonText = () => {
-    const {isDarkMode} = this.state
-    return isDarkMode ? 'Dark Mode' : 'Light Mode'
-  }
+
   render() {
     const {isDarkMode} = this.state
-    const buttonText = this.buttonText()
-    const backgroundChanges = isDarkMode ? 'DarkBackground' : 'LightBackground'
-    const heading = isDarkMode ? 'DarkHeading' : 'LightHeading'
-    const cardContainer = isDarkMode ? 'Dark' : 'Light'
+    const modeClassName = isDarkMode ? 'dark-mode' : 'light-mode'
+    const buttonText = isDarkMode ? 'Light Mode' : 'Dark Mode'
+
     return (
-      <div className="container">
-        <div className={cardContainer}>
-          <h1 className={heading}>Click To Change Mode</h1>
-          <button
-            type="button"
-            className={backgroundChanges}
-            onClick={this.backgroundChange}
-          >
+      <div className="app-container">
+        <div className={`container ${modeClassName}`}>
+          <h1 className="heading">Click To Change Mode</h1>
+          <button type="button" onClick={this.onClickButton} className="button">
             {buttonText}
           </button>
         </div>
