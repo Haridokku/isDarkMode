35fcd4c365fc25ee316f63f1ac70a758aeb57253
Haridokku <124425442+Haridokku@users.noreply.github.com>
2023-03-07 12:22:47 +0530

    final

--- src/components/LightDarkMode/index.js
+++ src/components/LightDarkMode/index.js
@@ -3,24 +3,27 @@ import {Component} from 'react'
 import './index.css'
 
 class LightDarkMode extends Component {
-  state = {
-    isDarkMode: true,
-  }
+  state = {isDarkMode: true}
 
-  onClickButton = () => {
+  backgroundChange = () => {
     this.setState(prevState => ({isDarkMode: !prevState.isDarkMode}))
   }
 
   render() {
     const {isDarkMode} = this.state
-    const modeClassName = isDarkMode ? 'dark-mode' : 'light-mode'
     const buttonText = isDarkMode ? 'Light Mode' : 'Dark Mode'
-
+    const backgroundChanges = isDarkMode ? 'DarkBackground' : 'LightBackground'
+    const heading = isDarkMode ? 'DarkHeading' : 'LightHeading'
+    const cardContainer = isDarkMode ? 'Dark' : 'Light'
     return (
-      <div className="app-container">
-        <div className={`container ${modeClassName}`}>
-          <h1 className="heading">Click To Change Mode</h1>
-          <button type="button" onClick={this.onClickButton} className="button">
+      <div className="container">
+        <div className={`changeContainer ${cardContainer}`}>
+          <h1 className={heading}>Click To Change Mode</h1>
+          <button
+            type="button"
+            className={backgroundChanges}
+            onClick={this.backgroundChange}
+          >
             {buttonText}
           </button>
         </div>
